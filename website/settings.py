5bc62eded0469ee5b494840ff603a45ef24301ab
ivo-bass <ivailo.ignatoff@gmail.com>
2020-12-29 00:42:34 +0200

    Fix some bg links

--- website/settings.py
+++ website/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "static"
+# STATIC_ROOT = BASE_DIR / "static"
 
 # media files
 MEDIA_URL = '/media/'
