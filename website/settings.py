126190ce53152bb37a12f9cd29975d249838d6b6
ivo-bass <ivailo.ignatoff@gmail.com>
2020-12-30 03:43:00 +0200

    add media

--- website/settings.py
+++ website/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-# STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "static"
 
 # media files
 MEDIA_URL = '/media/'
